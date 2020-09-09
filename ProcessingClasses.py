2b74e3b3efb7bfe3a360c2027fcf6c8437642fbe
Rupa Guha <guhasri@hotmail.com>
2020-09-08 23:33:11 -0700

    Add files via upload

---
+++ ProcessingClasses.py
@@ -0,0 +1,76 @@
+#------------------------------------------#
+# Title: Processing Classes
+# Desc: A Module for processing Classes
+# Change Log: (Who, When, What)
+# Rupa Guha, 2020-September-08, Created File
+#------------------------------------------#
+
+if __name__ == '__main__':
+    raise Exception('This file is not meant to ran by itself')
+
+import DataClasses as DC
+
+class DataProcessor:
+    """Processing the data in the application"""
+    @staticmethod
+    def add_CD(CDInfo, table):
+        """function to add CD info in CDinfo to the inventory table.
+
+
+        Args:
+            CDInfo (tuple): Holds information (ID, CD Title, CD Artist) to be added to inventory.
+            table (list of dict): 2D data structure (list of dicts) that holds the data during runtime.
+
+        Returns:
+            None.
+
+        """
+
+        cdId, title, artist = CDInfo
+        try:
+            cdId = int(cdId)
+        except:
+            raise Exception('ID must be an Integer!')
+        row = DC.CD(cdId, title, artist)
+        table.append(row)
+
+    @staticmethod
+    def select_cd(table: list, cd_idx: int) -> DC.CD:
+        """selects a CD object out of table that has the ID cd_idx
+
+        Args:
+            table (list): Inventory list of CD objects.
+            cd_idx (int): id of CD object to return
+
+        Raises:
+            Exception: If id is not in list.
+
+        Returns:
+            row (DC.CD): CD object that matches cd_idx
+
+        """
+        # TODO add code as required
+        pass
+
+
+    @staticmethod
+    def add_track(track_info: tuple, cd: DC.CD) -> None:
+        """adds a Track object with attributes in track_info to cd
+
+
+        Args:
+            track_info (tuple): Tuple containing track info (position, title, Length).
+            cd (DC.CD): cd object the tarck gets added to.
+
+        Raises:
+            Exception: DESCraised in case position is not an integer.
+
+        Returns:
+            None: DESCRIPTION.
+
+        """
+
+        # TODO add code as required
+        pass
+
+
